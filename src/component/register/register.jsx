e0eee71bccb6768a965092a790463f86a42b8d44
Thang cg <duongsag.hp11@gmail.com>
2022-10-21 09:23:29 +0700

    regiter done!

--- src/component/register/register.jsx
+++ src/component/register/register.jsx
@@ -1,135 +1,188 @@
-import React from "react";
+import React, { useState } from "react";
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Link, useNavigate } from "react-router-dom";
+import "./register.css";
+import axios from "axios";
+// import 'flowbite';
+
+const RegisterSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(4, "Tối thiểu 4 ký tự!")
+    .max(50, "Tối đa 50 ký tự!")
+    .required("Bắt buộc!"),
+
+  email: Yup.string()
+    .email("Email không hợp lệ!")
+    .required("Bắt buộc!")
+    .matches(
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+      "Email không đúng định dạng.(vd: abc@gmail.com)"
+    ),
+  password: Yup.string().required("Bắt buộc")
+  .matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+    "Mật khẩu phải ít nhất 8 ký tự ít nhất một viết hoa, một viết thường và một số."),
+    confirmPassword: Yup.string()
+      .required('Bắt buộc!')
+      .oneOf([Yup.ref("password"), null], "Mật khẩu không khớp!"),
+});
 
 export default function Registration() {
-    return (
-        <div>
-            <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gray-50">
-                <div>
-                    <a href="/">
-                        <h3 className="text-4xl font-bold text-purple-600">
-                            Logo
-                        </h3>
-                    </a>
-                </div>
-                <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
-                    <form>
-                        <div>
-                            <label
-                                htmlFor="name"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Name
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    type="text"
-                                    name="name"
-                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <label
-                                htmlFor="email"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Email
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    type="email"
-                                    name="email"
-                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <label
-                                htmlFor="password"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Password
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            <label
-                                htmlFor="password_confirmation"
-                                className="block text-sm font-medium text-gray-700 undefined"
-                            >
-                                Confirm Password
-                            </label>
-                            <div className="flex flex-col items-start">
-                                <input
-                                    type="password"
-                                    name="password_confirmation"
-                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                />
-                            </div>
-                        </div>
-                        <a
-                            href="#"
-                            className="text-xs text-purple-600 hover:underline"
-                        >
-                            Forget Password?
-                        </a>
-                        <div className="flex items-center mt-4">
-                            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                                Register
-                            </button>
-                        </div>
-                    </form>
-                    <div className="mt-4 text-grey-600">
-                        Already have an account?{" "}
-                        <span>
-                            <a className="text-purple-600 hover:underline" href="#">
-                                Log in
-                            </a>
-                        </span>
-                    </div>
-                    <div className="flex items-center w-full my-4">
-                        <hr className="w-full" />
-                        <p className="px-3 ">OR</p>
-                        <hr className="w-full" />
-                    </div>
-                    <div className="my-6 space-y-2">
-                        <button
-                            aria-label="Login with Google"
-                            type="button"
-                            className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 32 32"
-                                className="w-5 h-5 fill-current"
-                            >
-                                <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-                            </svg>
-                            <p>Login with Google</p>
-                        </button>
-                        <button
-                            aria-label="Login with GitHub"
-                            role="button"
-                            className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 32 32"
-                                className="w-5 h-5 fill-current"
-                            >
-                                <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-                            </svg>
-                            <p>Login with GitHub</p>
-                        </button>
-                    </div>
-                </div>
+  const [existedEmail, setExistedEmail] = useState("");
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    // confirmPassword: "",
+  });
+  const handleRegister = async (data) => {
+    return await axios.post("http://localhost:2212/api/auth/signup", data);
+  };
+  return (
+    <Formik
+      initialValues={form}
+      validationSchema={RegisterSchema}
+      onSubmit={(value) => {
+        handleRegister(value)
+          .then((res) => {
+            console.log(res);
+            if (res.data.success == true) {
+              // navigate("/login");
+            }
+          })
+          .catch((e) => console.log(e)
+          // setExistedEmail("Tài khoản đã tồn tại")
+          );
+      }}
+    >
+      <Form>
+        <section className="text-gray-600 body-font">
+          <div
+            className="container px-5 py-24 mx-auto flex flex-wrap items-center justify-center"
+            style={{
+              background:
+                'url("https://cdn.pixabay.com/photo/2017/08/19/19/43/nature-2659682_1280.jpg")',
+            }}
+          >
+            <div className="lg:w-3/5 lg:pr-0 pr-0">
+              <h1 className="title-font font-medium font-bold text-5xl text-white">
+                Slow-carb next level shoindcgoitch ethical authentic, poko
+                scenester
+              </h1>
+              <p className="leading-relaxed mt-4 text-white">
+                Poke slow-carb mixtape knausgaard, typewriter street art
+                gentrify hammock starladder roathse. Craies vegan tousled etsy
+                austin.
+              </p>
             </div>
-        </div>
-    );
+            <div className="lg:w-2/6 xl:w-2/5 md:w-2/3 bg-gray-100 rounded-lg p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0">
+              <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+                Đăng ký
+              </h2>
+              
+              {existedEmail ? (
+                <div class="ml-3 text-sm font-normal">
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  {existedEmail}
+                </div>
+              ) : null}
+              <div className="relative mb-4">
+                <label
+                  //   htmlFor="full-name"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Tên 
+                </label>
+                <Field
+                  type="text"
+                  name="name"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <a className="errors">
+                  <ErrorMessage name="name" />
+                </a>
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="full-name"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Email
+                </label>
+                <Field
+                  type="text"
+                  name="email"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <a className="errors">
+                  <ErrorMessage name="email" />
+                </a>
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Mật khẩu
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <a className="errors">
+                  <ErrorMessage name="password" />
+                </a>
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Xác nhận mật khẩu
+                </label>
+                <Field
+                  type="password"
+                  id="email"
+                  name="confirmPassword"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <a className="errors">
+                  <ErrorMessage name="confirmPassword" />
+                </a>
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              >
+                Button
+              </button>
+              <div>
+                <p className="text-xs text-gray-500 mt-3">
+                  Are you have a account ?
+                </p>
+                <Link style={{ color: "blue" }} to="/login">
+                  Log in
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      </Form>
+    </Formik>
+  );
 }
